740088cd0268b6e16bded75d1b0d7a5884b36607
Zohairajweh <60945291+Zohairajweh@users.noreply.github.com>
2020-05-14 15:58:12 -0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NET_Core_MVC_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
